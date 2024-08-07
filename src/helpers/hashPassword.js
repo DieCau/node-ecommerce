4798b07ebe58c01df64ec262882c7b3050b3eed0
Diego Caucota <diegocaucota@gmail.com>
2024-08-07 07:31:16 -0300

    add: emailRegex and hashPassword

---
+++ src/helpers/hashPassword.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10
+
+export const hashPassword = async(password) => {
+    const salt = await bcrypt.genSalt(saltRounds)
+    return bcrypt.hash(password, salt)
+}
