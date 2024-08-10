29c365c5f0d48356cd31342bcab0b1134bf8c130
Diego Caucota <diegocaucota@gmail.com>
2024-08-10 10:17:21 -0300

    add: productValidation and userValidation

---
+++ src/validators/productValidation.js
@@ -0,0 +1,5 @@
+import { body } from "express-validator";
+import { imageRegex } from "../helpers/imageURLRegex.js";
+import Product from "../models/product.model.js";
+
+export const productValidations = {};
