1464f13e407cafe17ab10bf353922029dfd08069
Diego Caucota <diegocaucota@gmail.com>
2024-08-12 09:23:23 -0300

    add: validateFields and validateToken

---
+++ src/validators/validateToken.js
@@ -0,0 +1,17 @@
+import { jwt } from "jsonwebtoken";
+import { SECRET } from "../config/config.js";
+
+export const validateToken = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    return res.status(401).json({ error: "Acceso denegado" });
+  }
+
+  try {
+    const verified = jwt.verify(token, SECRET);
+    req.userToken = verified
+    next()
+  } catch (error) {
+    res.status(400).json({ error: "Token invalido" });
+  }
+};
